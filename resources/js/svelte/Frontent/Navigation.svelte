9d7c140e529dfa72908d8e30027c83de3d29e21d
martincodes <62392843+martincodes-de@users.noreply.github.com>
2022-03-06 12:31:02 +0100

    update navbar background color

--- resources/js/svelte/Frontent/Navigation.svelte
+++ resources/js/svelte/Frontent/Navigation.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <!-- Responsive navbar-->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-darkgreen">
     <div class="container px-5">
         <a class="navbar-brand" href="/">Ukrainians find help</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
