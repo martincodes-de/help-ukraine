b4e67abe4f3dedb9ba5b3ae9b4197e420501844f
martincodes <62392843+martincodes-de@users.noreply.github.com>
2022-03-06 12:36:31 +0100

    add offer reports migration

---
+++ database/migrations/2022_03_06_111031_create_offer_reports_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offer_reports', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger("offer_id");
+            $table->foreign("offer_id")->references("id")->on("offers");
+
+            $table->string("reason");
+            $table->string("decision")->nullable();
+            $table->dateTime("solved_at");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offer_reports');
+    }
+};
