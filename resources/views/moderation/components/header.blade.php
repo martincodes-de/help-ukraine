b6d9acf24c58a4d18217374080984b035ff4511b
martincodes <62392843+martincodes-de@users.noreply.github.com>
2022-03-07 21:23:40 +0100

    update mod menu and naming of routes

--- resources/views/moderation/components/header.blade.php
+++ resources/views/moderation/components/header.blade.php
@@ -1,7 +1,13 @@
 <div class="container mt-2">
     <h5>Moderationtool</h5>
-    <div class="btn-group btn-group-sm" role="group">
+    <div class="btn-group btn-group-sm gx-2" role="group">
+        <a href="{{route("home")}}" target="_blank" class="btn btn-success">Homepage</a>
+        <a href="{{route("map")}}" target="_blank" class="btn btn-success">Public Map</a>
         <a href="{{route("moderation-all-offers")}}" class="btn btn-primary">All Offers</a>
     </div>
+    <form class="mt-1" method="post">
+        @csrf
+        <button class="btn btn-secondary btn-sm">Logout</button>
+    </form>
     <hr>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('frontend.homepage');
-});
+})->name("home");
 
 Route::get("/map", function () {
     return view("frontend.map");
-});
+})->name("map");
 
 Route::prefix("moderation")->group(function () {
     Route::get("offer/all", [OfferController::class, "showModerationList"])->name("moderation-all-offers");
